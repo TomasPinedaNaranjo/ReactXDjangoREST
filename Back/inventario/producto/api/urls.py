95445862f4818e948061bea076b601fbece3652e
TomasPinedaNaranjo <104210520+TomasPinedaNaranjo@users.noreply.github.com>
2025-06-17 17:56:49 -0500

    Test workflow

--- Back/inventario/producto/api/urls.py
+++ Back/inventario/producto/api/urls.py
@@ -2,5 +2,9 @@ from rest_framework.routers import DefaultRouter
 from producto.api.views import ProductoViewSet
 
 router = DefaultRouter()
+
+
 router.register(r'productos', ProductoViewSet, basename='producto')
+
+
 urlpatterns = router.urls
